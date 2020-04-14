a7a414ee2b62fefd06b7183cf5f2f3183e65f673
keithbooher <keith.booher@yahoo.com>
2020-04-13 20:39:02 -0500

    brows er router stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
+import { BrowserRouter , Route, withRouter } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo';
 import apolloClient from './config/createApolloClient';
 import { Users } from './modules/users';
 
+// import Home from "./somewhere"
+
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={apolloClient}>
-        <Users />
-      </ApolloProvider>
+      <BrowserRouter>
+        <ApolloProvider client={apolloClient}>
+            <UsersRoute /> 
+            {/* <Route exact path="/" component={Home} /> */}
+            {/* <Route exact path="/users" component={Users} /> */}
+        </ApolloProvider>
+      </BrowserRouter>
+
     )
   }
 }
+
+const determineUsersPage = (props) => {
+  const { location } = props;
+  if (location.pathname.match('/users')){
+    return <Users />
+  }
+  return (
+    null
+  )
+}
+
+const UsersRoute = withRouter(determineUsersPage)
 
 export default App;
